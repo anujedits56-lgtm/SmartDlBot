98a0667e6f0098190b2dd6497b95c244a583748c
anujedits42-byte <anujedits42@gmail.com>
2026-04-20 21:57:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
-# Replace these with your actual API details
-API_ID = "34446649"  # Replace with your API ID Get From my.telegram.org
-API_HASH = "8dc570c08d8e35e88fb9bfc73c65d7fa"  # Replace with your API Hash Get From my.telegram.org
-BOT_TOKEN = "8730656807:AAEb61aU6vTWCTkdpy6SLQ36T4m573fIoWQ"  # Replace with your Bot Token Get From @BotFather 
-# Spotify Configuration Get From https://developer.spotify.com/dashboard
-SPOTIFY_CLIENT_ID = '5941bb8af55d4345rf97f616e325'
-SPOTIFY_CLIENT_SECRET = '408frrwhhderroa9eff8'  # Replace this with your actual Spotify client secret
+import os
+API_ID = os.environ.get("API_ID", "34446649")
+API_HASH = os.environ.get("API_HASH", "8dc570c08d8e35e88fb9bfc73c65d7fa")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8730656807:AAEb61aU6vTWCTkdpy6SLQ36T4m573fIoWQ")
+SPOTIFY_CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID", "5941bb8af55d4345rf97f616e325")
+SPOTIFY_CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET", "408frrwhhderroa9eff8")
