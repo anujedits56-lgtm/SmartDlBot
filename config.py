45e7830e18b30a8b1c8df941bc2abd5416e2371f
anujedits42-byte <anujedits42@gmail.com>
2026-04-22 04:07:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,5 +19,3 @@ if not COMMAND_PREFIX:
 SPOTIFY_CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID", "5941bb8af55d4345rf97f616e325")
 SPOTIFY_CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET", "408f04b237add2ba1b8bfc5da9eff8")
 
-# YouTube cookies file path (if required for yt-dlp or similar)
-YT_COOKIES_PATH = "./cookies/ItsSmartToolBot.txt"
