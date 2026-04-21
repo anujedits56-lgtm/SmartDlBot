77f23069196a56cdee397c429548b1481c6d3698
anujedits42-byte <anujedits42@gmail.com>
2026-04-21 16:14:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,7 @@ import os
 API_ID = os.environ.get("API_ID", "34446649")
 API_HASH = os.environ.get("API_HASH", "8dc570c08d8e35e88fb9bfc73c65d7fa")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8730656807:AAEb61aU6vTWCTkdpy6SLQ36T4m573fIoWQ")
+ADMIN_IDS_RAW = os.getenv("ADMIN_IDS", "7892805795")
+MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0")
 SPOTIFY_CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID", "5941bb8af55d4345rf97f616e325")
 SPOTIFY_CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET", "408frrwhhderroa9eff8")
